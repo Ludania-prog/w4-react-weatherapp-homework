0c204cd1dc70b1646b3661fbaa08940278b816d4
Ludania-prog <ludania.pris@gmail.com>
2026-06-10 23:43:55 +0100

    css ajustments

--- src/City.js
+++ src/City.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function City() {
   return (
     <div className="City">
-      <h1>New York</h1>
+      <h2>New York</h2>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="weather-app">
-      <h1>Weather App</h1>
+      <h1 className="title">Weather App</h1>
       <Form />
       <City />
       <DayUpdate />
